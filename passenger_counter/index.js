49e7aa0a924590e92f1928a721016138ed606b31
Crayons001 <immanuelochi@gmail.com>
2023-03-02 23:30:47 +0300

    Added save and clear functionality

--- passenger_counter/index.js
+++ passenger_counter/index.js
@@ -1,6 +1,21 @@
 // document.getElementById("count-el").innerText = 5
+ 
+let countEl = document.getElementById("count-el")
+let count = 0
+let saveEl = document.getElementById("save-el")
 
+console.log(saveEl)
 function increment()
 {
-    var count
+    console.log("Clicked!")
+    count = count + 1
+    countEl.textContent = count
+}
+
+function save()
+{
+    let savedCount = count + " - "
+    saveEl.textContent += savedCount
+    count = countEl.textContent = 0
+    
 }
